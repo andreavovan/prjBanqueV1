cf1787263ae14dab1a993a1eb39244cfa08590b7
andreavovan <vovandrea2@gmail.com>
2025-10-16 16:59:51 +0200

    crr

--- compteclient.cpp
+++ compteclient.cpp
@@ -1,12 +1,27 @@
+/**
+  @file CompteClient.cpp
+  @brief Implémentation de la classe CompteClient
+  @version 1.0
+  @author Andrea vovan
+  @date 10/10/2025
+    */
+
 #include "compteclient.h"
 
-
+/**
+ * @brief CompteClient::CompteClient
+ * @param _nom
+ * @param _numero
+ */
 CompteClient::CompteClient(std::string _nom, int _numero): nom(_nom), numero(_numero)
 {
     compteEpargne = nullptr ;
     compteBancaire = new CompteBancaire (0);
 }
 
+/**
+ * @brief CompteClient::~CompteClient
+ */
 CompteClient::~CompteClient()
 {
     delete compteBancaire ;
